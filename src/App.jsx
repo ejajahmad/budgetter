4ea2443a27751ebaf604c609b676a2a0f44948c3
eahmad1 <eahmad@blackboardindia.com>
2022-09-26 11:15:17 +0530

    Expense Category Added

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,46 @@ function App() {
   const [viewAllExpense, setViewAllExpense] = useState(false);
   const [showModal, setShowModal] = useState(false);
 
+  const [expenseCategories, setExpenseCategories] = useState([
+    {
+      id: 1,
+      name: "Transportation",
+      icon: "/src/icons/bus-outline.svg",
+    },
+    {
+      id: 2,
+      name: "Food",
+      icon: "/src/icons/fast-food-outline.svg",
+    },
+    {
+      id: 3,
+      name: "Fun",
+      icon: "/src/icons/happy-outline.svg",
+    },
+    {
+      id: 4,
+      name: "Gadgets",
+      icon: "/src/icons/phone-portrait-outline.svg",
+    },
+    {
+      id: 5,
+      name: "Clothing",
+      icon: "/src/icons/shirt-outline.svg",
+    },
+    {
+      id: 6,
+      name: "Misc",
+      icon: "/src/icons/layers-outline.svg",
+    },
+  ]);
+
+  const [categoryId, setCategoryId] = useState("Transportation");
+
   const [currentExpense, setCurrentExpense] = useState({
     id: uuid(),
     title: "",
     amount: "",
+    category: "",
     date: Date.now(),
   });
 
@@ -27,18 +63,33 @@ function App() {
 
   const handleAddExpense = (e) => {
     e.preventDefault();
-    setExpenses((prev) => [...prev, currentExpense]);
+
+    const expense = {
+      ...currentExpense,
+      category: expenseCategories.filter(
+        (category) => category.name === categoryId
+      ),
+    };
+
+    setExpenses((prev) => [...prev, expense]);
     setBalance((prev) => prev - parseInt(currentExpense.amount));
+
     setCurrentExpense({
       id: uuid(),
       title: "",
       amount: "",
+      category: "",
       date: Date.now(),
     });
   };
 
   const handleRemoveExpense = (id) => {
     setExpenses((prev) => prev.filter((expense) => expense.id !== id));
+    setBalance(
+      (prev) =>
+        prev +
+        parseInt(expenses.filter((expense) => expense.id == id)[0].amount)
+    );
   };
 
   useEffect(() => {
@@ -46,7 +97,10 @@ function App() {
       console.log(
         "Expenses",
         expenses
-          .filter((expense) => new Date(expense.date).getDate() === new Date().getDate())
+          .filter(
+            (expense) =>
+              new Date(expense.date).getDate() === new Date().getDate()
+          )
           .reduce(function (previousValue, currentValue) {
             return +previousValue + parseInt(currentValue.amount);
           }, 0)
@@ -56,15 +110,25 @@ function App() {
 
   return (
     <div className=" p-2 h-screen  space-y-2 bg-[#1f2937]">
+      {/* Balance Change Modal */}
+
       {showModal && (
-        <Modal onHide={() => setShowModal(false)} confirm={() => setBalance(modalBalanceValue)}>
+        <Modal
+          onHide={() => setShowModal(false)}
+          confirm={() => setBalance(modalBalanceValue)}
+        >
           <h2>You Current Balance</h2>
           <div>
-            <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">
+            <label
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+            >
               Search
             </label>
             <div className="relative">
-              <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none text-white">₹</div>
+              <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none text-white">
+                ₹
+              </div>
               <input
                 type="number"
                 id="default-search"
@@ -78,9 +142,15 @@ function App() {
           </div>
         </Modal>
       )}
+
       <p className="text-2xl text-white text-center my-5">Budgetter</p>
+
+      {/* Balance Add Form */}
       <form onSubmit={handleAddBalance}>
-        <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">
+        <label
+          htmlFor="default-search"
+          className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+        >
           Search
         </label>
         <div className="relative">
@@ -92,9 +162,15 @@ function App() {
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
             </svg>
           </div>
+
           <input
             type="number"
             id="default-search"
@@ -117,7 +193,13 @@ function App() {
         <div className="flex items-center">
           <p className=" text-2xl flex items-center gap-2 flex-wrap">
             You Current Balance is:{" "}
-            <span className={`${balance < 0 ? "text-red-500 font-semibold" : "text-white"}`}>{internationalizeCurrency(balance)} </span>
+            <span
+              className={`${
+                balance < 0 ? "text-red-500 font-semibold" : "text-white"
+              }`}
+            >
+              {internationalizeCurrency(balance)}{" "}
+            </span>
             <button
               className=" ml-2 py-1 px-2 shadow-md no-underline rounded-full bg-green-500 hover:bg-green-600 text-white font-sans font-semibold  text-xs border-green-500 btn-primary hover:text-white hover:bg-green-500-light focus:outline-none active:shadow-none"
               onClick={() => setShowModal(true)}
@@ -125,23 +207,13 @@ function App() {
               Change *
             </button>
           </p>
-
-          {/* <button
-            className=" ml-2 py-1 px-2 shadow-md no-underline rounded-full bg-green-500 text-white font-sans font-semibold  text-xs border-green-500 btn-primary hover:text-white hover:bg-green-500-light focus:outline-none active:shadow-none"
-            onClick={() => ""}
-          >
-            Change *
-          </button>
-          <button
-            className=" ml-2 py-1 px-2 shadow-md no-underline rounded-full bg-lime-500 text-white font-sans font-semibold  text-xs border-lime-500 btn-primary hover:text-white hover:bg-lime-500-light focus:outline-none active:shadow-none"
-            onClick={() => ""}
-          >
-            Add +
-          </button> */}
         </div>
 
         <form onSubmit={handleAddExpense}>
-          <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300"
+          >
             Search
           </label>
           <div className="relative flex flex-col sm:flex-row gap-2">
@@ -153,15 +225,33 @@ function App() {
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                ></path>
               </svg>
             </div>
+            <select
+              className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              value={categoryId}
+              onChange={(e) => setCategoryId(e.target.value)}
+            >
+              {expenseCategories.map((category) => (
+                <option key={category.id} value={category.name}>
+                  {category.name}
+                </option>
+              ))}
+            </select>
             <input
               type="text"
               className="block p-4 sm:pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="What's you recent expense?"
               value={currentExpense.title}
-              onChange={(e) => setCurrentExpense({ ...currentExpense, title: e.target.value })}
+              onChange={(e) =>
+                setCurrentExpense({ ...currentExpense, title: e.target.value })
+              }
               required
             />
             <input
@@ -169,7 +259,9 @@ function App() {
               className="block p-4 sm:pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="How much did you spend?"
               value={currentExpense.amount}
-              onChange={(e) => setCurrentExpense({ ...currentExpense, amount: e.target.value })}
+              onChange={(e) =>
+                setCurrentExpense({ ...currentExpense, amount: e.target.value })
+              }
               required
             />
             <button
@@ -184,7 +276,9 @@ function App() {
         {expenses.length > 0 && (
           <div className="p-4  bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
             <div className="flex justify-between items-center mb-4">
-              <h3 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest Expenses</h3>
+              <h3 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+                Latest Expenses
+              </h3>
               <button
                 onClick={() => setViewAllExpense((prev) => !prev)}
                 href="#"
@@ -195,20 +289,27 @@ function App() {
             </div>
             {console.log(expenses)}
             <div className="flow-root">
-              <p className=" text-2xl flex items-center gap-2 flex-wrap">
+              <p className=" text-2xl flex items-center gap-2 flex-wrap text-gray-800 dark:text-white">
                 Today you spend:{" "}
                 <span className="text-red-500">
                   -
                   {internationalizeCurrency(
                     expenses
-                      .filter((expense) => new Date(expense.date).getDate() === new Date().getDate())
+                      .filter(
+                        (expense) =>
+                          new Date(expense.date).getDate() ===
+                          new Date().getDate()
+                      )
                       .reduce(function (previousValue, currentValue) {
                         return +previousValue + parseInt(currentValue.amount);
                       }, 0)
                   )}{" "}
                 </span>
               </p>
-              <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
+              <ul
+                role="list"
+                className="divide-y divide-gray-200 dark:divide-gray-700"
+              >
                 {expenses
                   .sort((a, b) => b.date - a.date)
                   .slice(0, viewAllExpense ? expenses.length : 5)
@@ -217,12 +318,27 @@ function App() {
                       <li className="py-3 sm:py-4" key={uuid()}>
                         <div className="flex items-center space-x-4">
                           <div className="flex-shrink-0">
-                            <div className="flex items-center justify-center rounded-full bg-gray-400 text-white  w-10 h-10 ">{expense.title[0]}</div>
+                            <div className="flex items-center justify-center rounded-full bg-gray-400 text-white  w-10 h-10 ">
+                              {expense?.category[0]?.icon ? (
+                                <img
+                                  src={expense.category[0].icon}
+                                  alt={expense.value}
+                                  className="w-5 h-5"
+                                />
+                              ) : (
+                                expense.title[0]
+                              )}
+                            </div>
                           </div>
                           <div className="flex-1 min-w-0">
-                            <p className="text-sm font-medium text-gray-900 truncate dark:text-white">{expense.title}</p>
+                            <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                              {expense.title}
+                            </p>
                             <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                              {formatRelative(addDays(expense.date, 0), new Date())}
+                              {formatRelative(
+                                addDays(expense.date, 0),
+                                new Date()
+                              )}
                             </p>
                           </div>
                           <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -2,16 +2,21 @@ import React from "react";
 
 export default function Modal({ onHide, confirm, heading, children }) {
   return (
-    <div class="bg-slate-800 bg-opacity-70 backdrop-blur flex justify-center items-center absolute top-0 right-0 bottom-0 left-0 z-50">
-      <div class="bg-white px-16 py-14 rounded-md text-center flex flex-col gap-5">
-        {heading && <h1 class="text-xl mb-4 font-bold text-slate-500">{heading}</h1>}
+    <div className="bg-slate-800 bg-opacity-70 backdrop-blur flex justify-center items-center absolute top-0 right-0 bottom-0 left-0 z-50">
+      <div className="bg-white px-16 py-14 rounded-md text-center flex flex-col gap-5">
+        {heading && (
+          <h1 className="text-xl mb-4 font-bold text-slate-500">{heading}</h1>
+        )}
         {children}
         <div>
-          <button class="bg-red-500 px-4 py-2 rounded-md text-md text-white" onClick={onHide}>
+          <button
+            className="bg-red-500 px-4 py-2 rounded-md text-md text-white"
+            onClick={onHide}
+          >
             Cancle
           </button>
           <button
-            class="bg-blue-500 px-7 py-2 ml-2 rounded-md text-md text-white font-semibold"
+            className="bg-blue-500 px-7 py-2 ml-2 rounded-md text-md text-white font-semibold"
             onClick={() => {
               confirm();
               onHide();
